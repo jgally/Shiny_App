069293fe1ead36234cae2b692b54c86b49b091ca
Jasmine Gallaway <gallyjas@gmail.com>
2023-12-11 02:32:02 -0500

    Major additions to the data exploration page on both the ui.r side and the server.r side

--- Final/server.R
+++ Final/server.R
@@ -8,7 +8,82 @@ library(shiny)
 library(shinydashboard)  
 library(tidyverse)  
 
-# Define server logic required to draw a histogram
-function(input, output, session) {
+#reading in data file  
+raw <- read.csv("abc-insurance-hr-data.csv", header = TRUE, sep = ",")  
 
+#Saving a dataset specifically to use for numeric summaries  
+num_data <- raw  
+
+#changing variables into meaningful factors  
+num_data$Ethnicity <- num_data$Ethnicity %>% 
+  factor(levels = c(0,1,2,3,4),
+         labels = c("White", "Black", "Asian", "Latino", "Other"))  
+
+num_data$Gender <- num_data$Gender %>% 
+  factor(levels = c(1,2),
+         labels = c("Female", "Male"))  
+
+num_data$JobRole <- num_data$JobRole %>% 
+  factor(levels = c(0,1,2,3,4,5,6,7),
+         labels = c("Administrative", "Customer Service", "Coordinator", "Jr Acct Mgr", "Sr Acct Mgr", "Asst Branch Mgr", "Branch Mgr", "Executive"))  
+
+
+
+# Define server logic required to draw a histogram
+function(input, output, session){
+  #Starting with the numeric summaries choices, the action button requires an observeEvent() to make sure it runs only when it is submitted  
+  observeEvent(input$num_button, {
+    #Saving user selections of which variable to summarize for easy tracking  
+    if("Age" %in% input$num_var){
+      focus <- paste0("Age")
+    }
+    if("Evaluation" %in% input$num_var){
+      focus <- paste0("Evaluation")
+    }
+    if("Salary" %in% input$num_var){
+      focus <- paste0("Salary.grade")
+    }
+    if("Job Satisfaction" %in% input$num_var){
+      focus <- paste0("Job.Satisfaction")
+    }
+    if("Intent to Quit" %in% input$num_var){
+      focus <- paste0("Intention.to.Quit")
+    }
+    
+    
+    #Saving type of summary  
+    type <- input$num_type  
+    
+    #Saving the numeric dataset as another object so as to not over write it  
+    user_num <- num_data  
+    
+    #Making a conditional argument to subset the data so the summary can be calculated  
+    if(input$num_subset == "Yes"){
+      if("Ethnicity" %in% input$num_sub_options){
+        user_num <- user_num[user_num$Ethnicity %in% input$num_sub_ethnicity, ]
+      }
+      if("Gender" %in% input$num_sub_options){
+        user_num <- user_num[user_num$Gender %in% input$num_sub_gender, ]
+      }
+      if("Job Role" %in% input$num_sub_options){
+        user_num <- user_num[user_num$JobRole %in% input$num_sub_roles, ]
+      }
+    }
+    
+    #Using if/else conditional arguments for the summary stat selection because only one option can be chosen at a time  
+    if(type == "Mean"){
+      calc <- round(mean(user_num[[focus]]), 3)
+    } else if(type == "Median"){
+      calc <- median(user_num[[focus]])
+    } else if(type == "Minimum"){
+      calc <- min(user_num[[focus]])
+    } else{
+      calc <- max(user_num[[focus]])
+    }
+    
+    #This here pastes the summary stat result in a sentence for the user  
+    output$sum_stat <- renderText({
+      paste0("The ", type, " for ", input$num_var, " based on your subsetting choices is ", calc, ".")
+    })
+  })
 }
--- Final/ui.R
+++ Final/ui.R
@@ -7,13 +7,39 @@ library(shiny)
 library(shinydashboard)  
 library(tidyverse)  
 #
-#Read in dataset file  
-#read.csv("abc-insurance-hr-data", sep = ",")
+#
+#reading in data file  
+raw <- read.csv("abc-insurance-hr-data.csv", header = TRUE, sep = ",")  
+
+#Saving a dataset specifically to use for numeric summaries  
+num_data <- raw  
+
+#changing variables into meaningful factors  
+num_data$Ethnicity <- num_data$Ethnicity %>% 
+  factor(levels = c(0,1,2,3,4),
+         labels = c("White", "Black", "Asian", "Latino", "Other"))  
+
+num_data$Gender <- num_data$Gender %>% 
+  factor(levels = c(1,2),
+         labels = c("Female", "Male"))  
+
+num_data$JobRole <- num_data$JobRole %>% 
+  factor(levels = c(0,1,2,3,4,5,6,7),
+         labels = c("Administrative", "Customer Service", "Coordinator", "Jr Acct Mgr", "Sr Acct Mgr", "Asst Branch Mgr", "Branch Mgr", "Executive"))  
+
+
+
 
 # Define UI using shiny dashboard  
 shinyUI(fluidPage(
+  #Using a bootswatch theme to change up the look  
+  tags$head(
+    tags$link(rel = "stylesheet", type = "text/css", href = "bootstrap.min.css")
+  ),
+  #Official content of the UI page
   dashboardPage(
   dashboardHeader(title = "Company Retention"),
+  #Here is my sidebar content
   dashboardSidebar(
     sidebarMenu(
       menuItem("About", 
@@ -35,15 +61,27 @@ shinyUI(fluidPage(
                            tabName = "predictions"))
     )
   ),
+  #Here is all the body content; there's a lot to go through!
   dashboardBody(
     tabItems(
       tabItem("about",
               h3("Thi")),
       tabItem("data",
               fluidRow(
+                #This first box is to describe the meaning of the numeric summaries; to give a key for what should be factor values  
                 box(
-                  title = "Numerical Summaries Inputs",
-                  "Below you can choose how you would like to explore the data. Some variables have been set to their numerical values rather than factoral values in order to have meaningful summaries.",
+                  title = "Key for Understanding",
+                  status = "warning",
+                  solidHeader = TRUE,
+                  width = 12,
+                  paste0("Remember: Variables such as evaluation, salary, job satisfaction, and intent to quit are ratings on a scale of 1 to 5 with 1 being a poorer score and 5 being a great score. See the About tab to determine specific meaning behind these scores.")
+                ),
+                #This box to hold all my numeric summary choices
+                box(
+                  title = "Numerical Summary Inputs",
+                  status = "success",
+                  solidHeader = TRUE, 
+                  "Below you can choose how you would like to explore numerical statistics for the data. Some variables have been set to their numerical values rather than factoral values in order to have meaningful summaries.",
                   #Choices will be set to their original numerical values only here in order to produce meaningful numerical summaries  
                   selectizeInput("num_var", "Choose a Variable to Summarize",
                                  choices = c("Age", "Evaluation", "Salary", "Job Satisfaction", "Intent to Quit"),
@@ -56,7 +94,7 @@ shinyUI(fluidPage(
                                c("Yes", "No, use all data"),
                                selected = "No, use all data"),
                   #This is the conditional panel to allow the user to select which rows of the data they would like to use in the numerical summaries (how they would like to subest the data)  
-                  conditionalPanel(condition = "input.num_subset == 'Yes'",
+                  conditionalPanel(condition = "input.num_subset != 'No, use all data'",
                                    checkboxGroupInput("num_sub_options", "Pick Which Variables to Subset the Data by (Pick one or more)",
                                                       choices = c("Ethnicity", "Gender", "Job Role"))),  
                   #The next conditional panels are based on the selections of the checkbox in the above conditional panel --Starting with with the options for ethnicity  
@@ -73,9 +111,62 @@ shinyUI(fluidPage(
                                    ),
                   #Here are the options if job role is selected  
                   conditionalPanel(condition = "input.num_sub_options.indexOf('Job Role') != -1",
-                                   wellPanel(checkboxGroupInput("num_sub_roles", "Choose One or More", choices = c("Administrative", "Customer Service", "Coordinator", "Junior Account Manager", "Senior Account Manager", "Assistant Branch Manager", "Branch Manager", "Executive"))
+                                   wellPanel(checkboxGroupInput("num_sub_roles", "Choose One or More", choices = c("Administrative", "Customer Service", "Coordinator", "Junior Account Manager" = "Jr Acct Mgr", "Senior Account Manager" = "Sr Acct Mgr", "Assistant Branch Manager" = "Asst Branch Mgr", "Branch Manager" = "Branch Mgr", "Executive"))
                                              )
-                  )
+                  ),
+                  #This action button is the submit for once all choices by the user are made  
+                  actionButton("num_button", "Generate Numeric Summaries", icon = icon("calculator"))
+                ),
+                
+                #Here is the numerical summaries box to go along with all the choices  
+                box(
+                  title = "Numerical Summaries",
+                  status = "success",
+                  solidHeader = TRUE,
+                  "New numeric summaries will generate here after the 'Generate Numeric Summaries' button has been clicked.",
+                  textOutput("sum_stat")
+                )
+              ),
+              br(),
+              fluidRow(
+                #This box is for user inputs on graphical summaries
+                box(
+                  title = "Graphical Summary Inputs",
+                  status = "info",
+                  solidHeader = TRUE,
+                  "Below you can choose how you would like to explore graphical summaries for the data. Variables have been set to their factoral values for meaningful axis labels.",
+                  radioButtons("graph", "Pick a Graph Type",
+                               choices = c("Jitter", "Barplot", "Density", "Count"),
+                               selected = "Jitter"),
+                  selectizeInput("x", "Pick the X-Axis Variable",
+                                 choices = c("Evaluation", "Salary", "Job Satisfaction", "Job Role", "Gender", "Ethnicity", "Age")),
+                  selectizeInput("y", "Pick the Y-Axis Variable", 
+                                 choices = c("Intent to Quit", "Job Satisfaction", "Salary", "Evaluation")),
+                  radioButtons("more", "Would you like to graph by a third variable?",
+                               choices = c("Yes", "No"),
+                               selected = "No"),
+                  conditionalPanel(condition = "input.third_var != 'No'",
+                                   wellPanel(
+                                     radioButtons("how", "How would you like to show the third variable?",
+                                                  choices = c("Color", "Side-by-side Graphing", "Subsetting Data"))
+                                   )),
+                  conditionalPanel(condition = "input.third_var != 'No'",
+                                   wellPanel(
+                                     selectizeInput("third_var", "What should be the third variable?",
+                                                    choices = c("Ethnicity", "Job Role", "Gender"))
+                                     
+                                   )),
+                  conditionalPanel(condition = "input.how == 'Subsetting Data'",
+                                   wellPanel(
+                                     
+                                   ))
+                ),
+                #This box is for the rendering of the plots after the user input
+                box(
+                  title = "Graphical Summary",
+                  status = "info",
+                  solidHeader = TRUE,
+                  "The new selected graph will generate here once the 'Graph It' button is pushed."
                 )
               )
               )
